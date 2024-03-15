d7157338b1a63d11c62dfcd35acbd501ce917549
Chukwuebuka Ibeh <cibeh@getcarbon.co>
2024-03-15 17:49:23 +0100

    real time fraud detection system implementation

---
+++ src/main/java/org/emysilva/FraudDetectionSystemApp.java
@@ -0,0 +1,38 @@
+package org.emysilva;
+
+import org.emysilva.fraud_detection_system.FraudDetectionSystem;
+import org.emysilva.fraud_detection_system.TransactionEvent;
+
+import java.io.*;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class FraudDetectionSystemApp {
+
+  private static final String FILE_NAME = "src/main/resources/dataset.txt";
+
+  public static void main(String[] args) {
+
+    FraudDetectionSystem fraudDetectionSystem = new FraudDetectionSystem();
+
+    // Simulate transaction stream (replace with actual stream processing)
+    // read data set from file
+    // file contains stale transaction data and may need to be updated
+    try (FileInputStream fis = new FileInputStream(FILE_NAME);
+         InputStreamReader isr = new InputStreamReader(fis);
+         BufferedReader br = new BufferedReader(isr)) {
+
+      String line;
+      while ((line = br.readLine()) != null) {
+        String[] words = line.split(",");
+        TransactionEvent event = new TransactionEvent(Instant.ofEpochSecond(Long.parseLong(words[words.length - 4])), Double.parseDouble(words[words.length - 3]), words[words.length - 2], words[words.length - 1]);
+        fraudDetectionSystem.processTransaction(event);
+      }
+    } catch (IOException e) {
+      System.out.println("An IO error occurred while reading file " + e.getMessage());
+    }
+
+  }
+}
+
---
+++ src/main/java/org/emysilva/fraud_detection_system/TransactionEvent.java
@@ -0,0 +1,33 @@
+package org.emysilva.fraud_detection_system;
+
+import java.time.Instant;
+
+public class TransactionEvent {
+    private Instant timestamp;
+    private double amount;
+    private String userID;
+    private String serviceID;
+
+    public TransactionEvent(Instant timestamp, double amount, String userID, String serviceID) {
+        this.timestamp = timestamp;
+        this.amount = amount;
+        this.userID = userID;
+        this.serviceID = serviceID;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getServiceID() {
+        return serviceID;
+    }
+}
